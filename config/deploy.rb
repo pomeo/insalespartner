ab0e31e063766a682b77a105ab8b512c8e00e542
Sergey Ovechkin <me@sovechkin.com>
2014-01-17 16:31:59 +0400

    настроен capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,19 @@
 #========================
 #CONFIG
 #========================
-set :application, "site"
+set :application, "partner.insales.sovechkin.com"
 #========================
 #CONFIG
 #========================
 require           "capistrano-offroad"
 offroad_modules   "defaults", "supervisord"
-set :repository,  "git@github.com:user/#{application}.git"
-set :supervisord_start_group, "name"
-set :supervisord_stop_group, "name"
+set :repository,  "git@github.com:pomeo/insalespartner.git"
+set :supervisord_start_group, "partner"
+set :supervisord_stop_group, "partner"
 #========================
 #ROLES
 #========================
 set  :gateway,    "#{application}"  # main server
-role :app,        "ubuntu@10.3.x.x" # lxc container
+role :app,        "ubuntu@10.3.10.130" # lxc container
  
 after "deploy:create_symlink", "deploy:npm_install", "deploy:restart"
