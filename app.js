d089f052c1dd22d179ce933aae67277958b04c0a
Sergey Ovechkin <me@sovechkin.com>
2014-11-28 20:51:09 +0300

    fix bodyparser

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ app.enable('trust proxy');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 app.use(session({
   store: new RedisStore({
